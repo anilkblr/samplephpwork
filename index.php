cf1702cca9481b1fb1ee8250f8032a8d72cc823a
anilkblr <38598323+anilkblr@users.noreply.github.com>
2018-04-22 09:27:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>Developing PHP Application for class version 1 </h1>
+<h1>Developing PHP Application for class version2 added lead approval </h1>
 
 <?php
 echo "Version: 1.1";
